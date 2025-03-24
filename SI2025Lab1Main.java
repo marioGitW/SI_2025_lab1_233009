61a5ea7f97eece1d2b6e545e879d8507298a96a8
Mario Spasovski <mario.spaska@gmail.com>
2025-03-24 23:47:41 +0100

    Mark a task as completed by name

--- SI2025Lab1Main.java
+++ SI2025Lab1Main.java
@@ -104,11 +104,17 @@ class TaskManager {
     // 8. Mark a task as completed by name
     public void markTaskCompleted(String name) {
         // TODO: Implement completion logic
+        for (Task task : tasks) {
+            if (task.getName().equals(name)) {
+                task.complete();
+            }
+        }
     }
 
     // 9. Mark all tasks in a category as completed
     public void markCategoryCompleted(String category) {
         // TODO: Implement bulk completion logic
+
     }
 }
 
